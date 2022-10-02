68f9a9131667eb00ede5eccc88e1258f6eeccf69
Artyom Maisiuk <maisiukartyom@gmail.com>
2022-10-02 12:24:29 +0300

    added ITracer and TraceResult class

--- Tracer/Tracer.cs
+++ Tracer/Tracer.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using System.Diagnostics;
+
+namespace TracerApp
+{
+    public class TraceResult
+    {
+        public Stopwatch stopwatch;
+
+        public long time { get; set; }
+
+        public string methodName { get; set; }
+
+        public string className { get; set; }
+
+        public List<TraceResult> traceResultList { get; set; }
+    }
+    public interface ITracer
+    {
+
+        void StartTrace();
+
+        void StopTrace();
+
+        string[] GetTraceResult();
+
+    }
+}
